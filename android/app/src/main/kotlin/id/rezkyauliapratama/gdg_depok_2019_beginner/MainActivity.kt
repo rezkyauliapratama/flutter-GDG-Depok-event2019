450d0cd5c7013630fdb7b77e1c29d83bd6e38c9f
Rezky Aulia Pratama <rezky.pratama@dana.id>
2019-08-21 17:44:54 +0700

    first commit

---
+++ android/app/src/main/kotlin/id/rezkyauliapratama/gdg_depok_2019_beginner/MainActivity.kt
@@ -0,0 +1,13 @@
+package id.rezkyauliapratama.gdg_depok_2019_beginner
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
